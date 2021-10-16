5a78d03160bcd38aab5c49d4277a4a083d3f36c5
huazhouwang <huazhou19@gmail.com>
2021-10-16 17:15:00 +0800

    Enable EIP1559

--- src/widgets/gas-price/index.tsx
+++ src/widgets/gas-price/index.tsx
@@ -1,8 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import View from './view';
+
+const parsePrice = (eip1559Enabled: boolean, price: any): string => {
+  if (!price) {
+    return '--';
+  } else if (eip1559Enabled) {
+    return `${price.maxPriorityFeePerGas}/${price.maxFeePerGas}`;
+  } else {
+    return `${price.price} GWEI`;
+  }
+};
 
 const BlockNativeGasWidget = () => {
   const [gasPrices, setGasPrices] = useState<any>();
+  const [eip1559Enabled, setEIP1559Enabled] = useState<boolean>(false);
+  const switchEIP1559Enabled = useCallback(() => {
+    const newValue = !eip1559Enabled;
+    chrome.storage.sync.set({ eip1559Enabled: newValue }, () =>
+      setEIP1559Enabled(() => newValue),
+    );
+  }, [eip1559Enabled, setEIP1559Enabled]);
+
+  useEffect(() => {
+    if (!setEIP1559Enabled) {
+      return;
+    }
+
+    chrome.storage.sync.get('eip1559Enabled', (data) =>
+      setEIP1559Enabled(data?.eip1559Enabled === true),
+    );
+    const listener = (changes: any, area: any) => {
+      if (typeof changes.eip1559Enabled?.newValue !== 'undefined') {
+        setEIP1559Enabled(changes.eip1559Enabled.newValue === true);
+      }
+    };
+    chrome.storage.onChanged.addListener(listener);
+
+    return () => listener && chrome.storage.onChanged.removeListener(listener);
+  }, [setEIP1559Enabled]);
 
   useEffect(() => {
     if (!setGasPrices) {
@@ -14,8 +49,7 @@ const BlockNativeGasWidget = () => {
         return;
       }
 
-      const [rapid, fast, standard] =
-        blockNativeGasEstimator.estimatedPrices.map((i: any) => i.price);
+      const [rapid, fast, standard] = blockNativeGasEstimator.estimatedPrices;
       setGasPrices({ rapid, fast, standard });
     };
 
@@ -33,12 +67,14 @@ const BlockNativeGasWidget = () => {
     return () => listener && chrome.storage.onChanged.removeListener(listener);
   }, [setGasPrices]);
 
-  let { rapid, fast, standard } = gasPrices || {};
+  const { rapid, fast, standard } = gasPrices || {};
   return (
     <View
-      rapid={rapid}
-      fast={fast}
-      standard={standard}
+      eip1559Enabled={eip1559Enabled}
+      switchEIP1559Enabled={switchEIP1559Enabled}
+      rapid={parsePrice(eip1559Enabled, rapid)}
+      fast={parsePrice(eip1559Enabled, fast)}
+      standard={parsePrice(eip1559Enabled, standard)}
       onClick={() =>
         chrome.tabs.create({
           url: 'https://www.blocknative.com/gas-estimator',
--- src/widgets/gas-price/view.tsx
+++ src/widgets/gas-price/view.tsx
@@ -1,22 +1,24 @@
 import styled from 'styled-components';
-import { Column, SizedBox, Row, SmallestRoundedWidget } from '../basic/basic';
+import { Column, Row, SmallestRoundedWidget } from '../basic/basic';
 import rapidLogo from '../../assets/logo_rapid.svg';
 import logoFast from '../../assets/logo_fast.svg';
 import logoStandard from '../../assets/logo_standard.svg';
 
-const WidgetContainer = styled(SmallestRoundedWidget)`
+const PriceList = styled(Column)`
+  cursor: pointer;
   justify-content: space-between;
 `;
 
 const PriceContainer = styled(Row)`
   width: 100%;
-  height: 48px;
+  height: 40px;
   align-items: center;
 `;
 
 const PriceLogo = styled.img`
   width: 32px;
   height: 32px;
+  margin-right: 8px;
 `;
 
 const PriceLabel = styled.span`
@@ -29,6 +31,34 @@ const PriceValue = styled.span`
   font-size: 14px;
 `;
 
+const EIP1559FeeContainer = styled(Row)`
+  align-items: baseline;
+`;
+
+const PriorityFee = styled(PriceValue)`
+  font-size: 9px;
+`;
+
+const EIP1559StatusContainer = styled(Row)`
+  cursor: pointer;
+  width: fit-content;
+  align-items: center;
+  align-self: flex-end;
+  margin-top: -6px;
+  margin-right: -6px;
+  padding: 1px 6px 1px 6px;
+  border-radius: 8px;
+
+  &:hover {
+    background-color: #424242;
+  }
+`;
+
+const EIP1559StatusText = styled.span`
+  font-size: 9px;
+  color: #9e9e9e;
+`;
+
 const Price = ({
   label,
   value,
@@ -37,33 +67,71 @@ const Price = ({
   label: string;
   value: string;
   logo: string;
-}) => (
-  <PriceContainer>
-    <PriceLogo src={logo} />
-    <SizedBox width={16} />
-    <Column>
-      <PriceValue>{value || '--'} GWEI</PriceValue>
-      <PriceLabel>{label}</PriceLabel>
-    </Column>
-  </PriceContainer>
-);
+}) => {
+  const isEIP1559 = value && value.indexOf('/') >= 0;
+  const legacyPrice = isEIP1559 ? '' : value;
+  const [priorityFee, maxFee] = isEIP1559 ? value.split('/') : [];
+
+  return (
+    <PriceContainer>
+      <PriceLogo src={logo} />
+      <Column>
+        {isEIP1559 ? (
+          <EIP1559FeeContainer>
+            <PriceValue>{maxFee}</PriceValue>
+            <PriorityFee> {priorityFee}</PriorityFee>
+          </EIP1559FeeContainer>
+        ) : (
+          <PriceValue>{legacyPrice || '--'}</PriceValue>
+        )}
+        <PriceLabel>{label}</PriceLabel>
+      </Column>
+    </PriceContainer>
+  );
+};
+
+const EIP1559Status = ({
+  eip1559Enabled,
+  switchEIP1559Enabled,
+}: {
+  eip1559Enabled: boolean;
+  switchEIP1559Enabled: () => void;
+}) => {
+  return (
+    <EIP1559StatusContainer onClick={switchEIP1559Enabled}>
+      <EIP1559StatusText>
+        {eip1559Enabled ? 'EIP1559' : 'Legacy'}
+      </EIP1559StatusText>
+    </EIP1559StatusContainer>
+  );
+};
 
 const View = ({
+  eip1559Enabled,
+  switchEIP1559Enabled,
   rapid,
   fast,
   standard,
   onClick,
 }: {
+  eip1559Enabled: boolean;
+  switchEIP1559Enabled: () => void;
   rapid: string;
   fast: string;
   standard: string;
   onClick: () => void;
 }) => (
-  <WidgetContainer onClick={onClick}>
-    <Price label={'~15s'} logo={rapidLogo} value={rapid} />
-    <Price label={'~1min'} logo={logoFast} value={fast} />
-    <Price label={'~3min'} logo={logoStandard} value={standard} />
-  </WidgetContainer>
+  <SmallestRoundedWidget>
+    <EIP1559Status
+      eip1559Enabled={eip1559Enabled}
+      switchEIP1559Enabled={switchEIP1559Enabled}
+    />
+    <PriceList onClick={onClick}>
+      <Price label={'~15s'} logo={rapidLogo} value={rapid} />
+      <Price label={'~1min'} logo={logoFast} value={fast} />
+      <Price label={'~3min'} logo={logoStandard} value={standard} />
+    </PriceList>
+  </SmallestRoundedWidget>
 );
 
 export default View;
